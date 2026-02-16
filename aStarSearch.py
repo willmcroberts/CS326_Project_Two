b86d3429ea2914b87c439eef3b988df556733f30
willmcroberts <williammcroberts15@gmail.com>
2026-02-16 16:16:27 -0500

    Add Grid class

--- aStarSearch.py
+++ aStarSearch.py
@@ -5,3 +5,34 @@ import random
 import time
 import heapq
 import sys
+class Grid:
+    def __init__(self, m, n, start, goal, min_cost, max_cost):
+        self.m = m
+        self.n = n
+        self.start = start
+        self.goal = goal
+        self.min_cost = min_cost
+        self.max_cost = max_cost
+        self.costs = {}
+        self._assign_costs()
+
+    def _assign_costs(self):
+        for r in range(self.m):
+            for c in range(self.n):
+                for dr, dc in [(1,0),(-1,0),(0,1),(0,-1)]:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < self.m and 0 <= nc < self.n:
+                        self.costs[((r,c),(nr,nc))] = random.randint(
+                            self.min_cost, self.max_cost
+                        )
+
+    def neighbors(self, node):
+        r, c = node
+        for dr, dc in [(1,0),(-1,0),(0,1),(0,-1)]:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < self.m and 0 <= nc < self.n:
+                yield (nr, nc)
+
+    def cost(self, a, b):
+        return self.costs[(a, b)]
+
